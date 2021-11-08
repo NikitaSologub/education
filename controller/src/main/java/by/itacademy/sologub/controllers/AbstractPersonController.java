f0fec333981eaa54315d060425cdc0c6ac556ca3
nikita <nikitasologub42@gmail.com>
2021-11-08 19:02:33 +0300

    AbstractPersonController is added

---
+++ controller/src/main/java/by/itacademy/sologub/controllers/AbstractPersonController.java
@@ -0,0 +1,77 @@
+package by.itacademy.sologub.controllers;
+
+import by.itacademy.sologub.User;
+import lombok.extern.slf4j.Slf4j;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static by.itacademy.sologub.constants.Attributes.LOGIN;
+
+@Slf4j
+public abstract class AbstractPersonController<T extends User> extends BaseController {
+
+    protected abstract String getRole();
+
+    protected abstract String getUrl();
+
+    protected abstract boolean putInRepo(T user);
+
+    protected abstract boolean changeInRepo(T user);
+
+    protected abstract boolean deleteInRepo(T user);
+
+    protected abstract boolean deleteInRepo(String userLogin);
+
+    protected abstract T extractUserFromForm(HttpServletRequest req);
+
+    protected abstract T extractUserFromFormWithIds(HttpServletRequest req);
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        T user = extractUserFromForm(req);
+        boolean result = putInRepo(user);
+
+        String msg;
+        if (result) {
+            msg = getRole() + " " + req.getParameter(LOGIN) + " успешно добавлен";
+            log.info("{} {} успешно добавлен", getRole(), req.getParameter(LOGIN));
+        } else {
+            msg = "Не удалось добавить " + getRole() + " " + req.getParameter(LOGIN);
+            log.info("Не удалось добавить {} {}", getRole(), req.getParameter(LOGIN));
+        }
+        forward(getUrl(), msg, req, resp);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        T user = extractUserFromFormWithIds(req);
+        boolean result = changeInRepo(user);
+        String msg;
+        if (result) {
+            msg = getRole() + " " + req.getParameter(LOGIN) + " успешно изменён";
+            log.info("{} {} успешно изменён", getRole(), req.getParameter(LOGIN));
+        } else {
+            msg = "Не удалось изменить " + getRole() + " " + req.getParameter(LOGIN);
+            log.info("Не удалось изменить {} {}", getRole(), req.getParameter(LOGIN));
+        }
+        forward(getUrl(), msg, req, resp);
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        boolean result = deleteInRepo(req.getParameter(LOGIN));
+        String msg;
+        if (result) {
+            msg = getRole() + " " + req.getParameter(LOGIN) + " успешно удалён";
+            log.info("{} {} успешно удалён", getRole(), req.getParameter(LOGIN));
+        } else {
+            msg = "Не удалось удалить " + getRole() + " " + req.getParameter(LOGIN);
+            log.info("Не удалось удалить {} {}", getRole(), req.getParameter(LOGIN));
+        }
+        forward(getUrl(), msg, req, resp);
+    }
+}
--- controller/src/main/java/by/itacademy/sologub/controllers/TeacherController.java
+++ controller/src/main/java/by/itacademy/sologub/controllers/TeacherController.java
@@ -3,13 +3,11 @@ package by.itacademy.sologub.controllers;
 import by.itacademy.sologub.Credential;
 import by.itacademy.sologub.Teacher;
 import by.itacademy.sologub.TeacherRepo;
+import by.itacademy.sologub.role.Role;
 import lombok.extern.slf4j.Slf4j;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.time.LocalDate;
 
 import static by.itacademy.sologub.constants.Attributes.FIRSTNAME;
@@ -26,25 +24,43 @@ import static by.itacademy.sologub.constants.Constant.TEACHER_REPO;
 
 @WebServlet(TEACHER_CONTROLLER)
 @Slf4j
-public class TeacherController extends BaseController {
+public class TeacherController extends AbstractPersonController<Teacher> {
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
-        Teacher teacher = extractTeacherFromForm(req);
-        boolean result = repo.putTeacherIfNotExists(teacher);
-
-        String msg;
-        if (result) {
-            msg = "Учитель " + req.getParameter(LOGIN) + " успешно добавлен";
-            log.info("Учитель {} успешно добавлен", req.getParameter(LOGIN));
-        } else {
-            msg = "Не удалось добавить Учителя " + req.getParameter(LOGIN);
-            log.info("Не удалось добавить учителя {}", req.getParameter(LOGIN));
-        }
-        forward(ADMIN_TEACHERS_PAGE, msg, req, resp);
+    protected String getRole() {
+        return String.valueOf(Role.TEACHER);
     }
 
-    Teacher extractTeacherFromForm(HttpServletRequest req) {
+    @Override
+    protected String getUrl() {
+        return ADMIN_TEACHERS_PAGE;
+    }
+
+    @Override
+    protected boolean putInRepo(Teacher user) {
+        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
+        return repo.putTeacherIfNotExists(user);
+    }
+
+    @Override
+    protected boolean changeInRepo(Teacher user) {
+        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
+        return repo.changeTeachersParametersIfExists(user);
+    }
+
+    @Override
+    protected boolean deleteInRepo(Teacher user) {
+        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
+        return repo.deleteTeacher(user);
+    }
+
+    @Override
+    protected boolean deleteInRepo(String userLogin) {
+        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
+        return repo.deleteTeacher(userLogin);
+    }
+
+    @Override
+    protected Teacher extractUserFromForm(HttpServletRequest req) {
         Teacher teacher = new Teacher()
                 .withCredential(new Credential()
                         .withLogin(req.getParameter(LOGIN))
@@ -58,42 +74,12 @@ public class TeacherController extends BaseController {
         return teacher;
     }
 
-    Teacher extractTeacherFromFormWithIds(HttpServletRequest req){
-        Teacher teacher = extractTeacherFromForm(req);
+    @Override
+    protected Teacher extractUserFromFormWithIds(HttpServletRequest req) {
+        Teacher teacher = extractUserFromForm(req);
         teacher.setId(Integer.parseInt(req.getParameter(ID)));
         teacher.getCredential().setId(Integer.parseInt(req.getParameter(CREDENTIAL_ID)));
         log.debug("Из запроса извлечён обьект учителя {}", teacher);
         return teacher;
-    }
-
-    @Override
-    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
-        Teacher newTeacherValues = extractTeacherFromFormWithIds(req);
-        boolean result = repo.changeTeachersParametersIfExists(newTeacherValues);
-        String msg;
-        if (result) {
-            msg = "Учитель " + req.getParameter(LOGIN) + " успешно изменён";
-            log.info("Учитель {} успешно изменён", req.getParameter(LOGIN));
-        } else {
-            msg = "Не удалось изменить учителя " + req.getParameter(LOGIN);
-            log.info("Не удалось изменить учителя {}", req.getParameter(LOGIN));
-        }
-        forward(ADMIN_TEACHERS_PAGE, msg, req, resp);
-    }
-
-    @Override
-    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        TeacherRepo repo = (TeacherRepo) getServletContext().getAttribute(TEACHER_REPO);
-        boolean result = repo.deleteTeacher(req.getParameter(LOGIN));
-        String msg;
-        if (result) {
-            msg = "Учитель " + req.getParameter(LOGIN) + " успешно удалён";
-            log.info("Учитель {} успешно удалён", req.getParameter(LOGIN));
-        } else {
-            msg = "Не удалось удалить чителя " + req.getParameter(LOGIN);
-            log.info("Не удалось удалить учителя {}", req.getParameter(LOGIN));
-        }
-        forward(ADMIN_TEACHERS_PAGE, msg, req, resp);
     }
 }
