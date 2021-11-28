4c7edf180f257bc5ff955ee89490fa8737b5f826
nikita <nikitasologub42@gmail.com>
2021-11-28 23:34:54 +0300

    model class Group hibernate annotations created
    hibernate cfg xml class Group mapping added

--- model/src/main/java/by/itacademy/sologub/Group.java
+++ model/src/main/java/by/itacademy/sologub/Group.java
@@ -4,6 +4,16 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Objects;
@@ -12,12 +22,42 @@ import java.util.Set;
 @Data
 @NoArgsConstructor
 @ToString(callSuper = true)
+@NamedQueries({
+        @NamedQuery(name = "getGroupById", query = "select g from Group g where g.id=:id"),
+        @NamedQuery(name = "deleteGroupById", query = "delete from Group g where g.id=:id")})
+@Entity
+@Table(name = "group", schema = "public")
 public class Group extends AbstractEntity {
+    @Column(unique = true)
     private String title;
+    @ManyToOne(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "teacher_id")
     private Teacher teacher;
     private String description;
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "group_subject",
+            joinColumns = {@JoinColumn(name = "group_id")}, inverseJoinColumns = {@JoinColumn(name = "subject_id")})
     private Set<Subject> subjects = new HashSet<>();
+    @ManyToMany(cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinTable(name = "group_student",
+            joinColumns = {@JoinColumn(name = "group_id")}, inverseJoinColumns = {@JoinColumn(name = "student_id")})
     private Set<Student> students = new HashSet<>();
+
+    public boolean addStudent(Student s) {
+        return this.students.add(s);
+    }
+
+    public boolean removeStudent(Student s) {
+        return this.students.remove(s);
+    }
+
+    public boolean addSubject(Subject s) {
+        return this.subjects.add(s);
+    }
+
+    public boolean removeSubject(Subject s) {
+        return this.subjects.remove(s);
+    }
 
     public Group withId(int id) {
         setId(id);
@@ -39,12 +79,12 @@ public class Group extends AbstractEntity {
         return this;
     }
 
-    public Group withStudents(Student ... arr){
+    public Group withStudents(Student... arr) {
         students.addAll(Arrays.asList(arr));
         return this;
     }
 
-    public Group withSubjects(Subject ... arr){
+    public Group withSubjects(Subject... arr) {
         subjects.addAll(Arrays.asList(arr));
         return this;
     }
