a2ebc471197e78d1121e0dc80a51cfce49b811b1
nikita <nikitasologub42@gmail.com>
2022-02-07 00:25:32 +0300

    student function to see own marks was added
    MarkControllerForStudent was added
    student_all_marks_page jsp was added

---
+++ controller/src/main/java/by/itacademy/sologub/controllers/MarkControllerForStudent.java
@@ -0,0 +1,35 @@
+package by.itacademy.sologub.controllers;
+
+import by.itacademy.sologub.model.Mark;
+import by.itacademy.sologub.model.Student;
+import by.itacademy.sologub.services.MarkService;
+import by.itacademy.sologub.services.StudentService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
+import java.util.Set;
+
+import static by.itacademy.sologub.constants.Attributes.STUDENT;
+import static by.itacademy.sologub.constants.Constant.OBJECTS_SET;
+
+@Slf4j
+@RequiredArgsConstructor
+@Controller
+public class MarkControllerForStudent {
+    private final MarkService markService;
+    private final StudentService studentService;
+
+    @GetMapping("marks")
+    public ModelAndView getAllMarks(Principal principal) {
+        Student student = studentService.getStudentIfExistsOrGetSpecialValue(principal.getName());
+        Set<Mark> markSet = markService.getAllMarksByStudentId(student.getId());
+        ModelAndView mav = new ModelAndView("student_all_marks_page");
+        mav.getModel().put(STUDENT, student);
+        mav.getModel().put(OBJECTS_SET, markSet);
+        return mav;
+    }
+}
